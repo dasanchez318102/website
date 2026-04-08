fd6a79737403d3ff2599151b87ed5f897c834b9b
David <dasanchez318102@gmail.com>
2026-04-07 23:03:44 -0700

    new look

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click",function(){
+    mainNav.classList.toggle("open");
+
+
+});
